428d2a18e9572b95e1699082f7166af534c55a08
Ozkar L. Garcell <ozkar.garcell@gmail.com>
2021-12-22 14:23:37 -0500

    feat: included fastapi metadata and medications router

--- drones/app/main.py
+++ drones/app/main.py
@@ -3,10 +3,23 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from app.settings import get_settings
 from app.database import create_db_and_tables
-from app.routers import drones
+from app.routers import drones, medications
 
 settings = get_settings()
-app = FastAPI(debug=settings.debug, title=settings.project_name)
+app = FastAPI(
+    debug=settings.debug,
+    title=settings.project_name,
+    description="Musala Soft Practical Task: **Drones API**",
+    version="0.1",
+    contact={
+        "name": "Oscar L. Garcell",
+        "url": "https://github.com/codeshard/drones-api",
+    },
+    license_info={
+        "name": "MIT",
+        "url": "https://mit-license.org/",
+    },
+)
 
 origins = [
     "http://127.0.0.1",
@@ -27,3 +40,4 @@ async def startup():
 
 
 app.include_router(drones.router)
+app.include_router(medications.router)
