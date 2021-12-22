a853c892e8caa155ded3387dd564fce48f854a2c
Ozkar L. Garcell <ozkar.garcell@gmail.com>
2021-12-21 23:43:31 -0500

    fix: switched to async engine provided by sqlalchemy

--- drones/app/database.py
+++ drones/app/database.py
@@ -1,14 +1,20 @@
-from sqlmodel import SQLModel, create_engine
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from sqlmodel import SQLModel
 
 from app.settings import get_settings
 
 settings = get_settings()
 
-engine = create_engine(
-    url=settings.database_dsn,
-    echo=settings.debug,
-)
+engine = create_async_engine(settings.database_dsn, echo=True, future=True)
 
 
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
+async def create_db_and_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
+
+
+async def get_session() -> AsyncSession:
+    async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+    async with async_session() as session:
+        yield session
