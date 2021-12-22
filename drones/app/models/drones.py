a38dd3279d8d6248b070928837ea8e50457bfa7c
Ozkar L. Garcell <ozkar.garcell@gmail.com>
2021-12-22 09:11:38 -0500

    feat: drones models, WiP

---
+++ drones/app/models/drones.py
@@ -0,0 +1,30 @@
+from sqlmodel import Field
+from enum import Enum, auto
+from app.models import BaseModel
+from sqlalchemy import UniqueConstraint
+
+
+class DroneModel(int, Enum):
+    LIGHTWEIGHT = auto()
+    MIDDLEWEIGHT = auto()
+    CRUISERWEIGHT = auto()
+    HEAVYWEIGHT = auto()
+
+
+class DroneState(int, Enum):
+    IDLE = auto()
+    LOADING = auto()
+    LOADED = auto()
+    DELIVERING = auto()
+    DELIVERED = auto()
+    RETURNING = auto()
+
+
+class Drone(BaseModel, table=True):
+    __tablename__ = "drones"
+    __table_args__ = (UniqueConstraint("serial_number"),)
+    serial_number: str = Field(..., max_length=100, index=True)
+    model: DroneModel = DroneModel.LIGHTWEIGHT
+    weight_limit: float = Field(..., ge=0, le=500)
+    battery_capacity: float = Field(..., ge=0, le=100)
+    state: DroneState = DroneState.IDLE
